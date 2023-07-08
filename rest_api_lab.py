cb9590e0b4b94ff0f8f7d9c11a048507d7ca275e
yjb23tech <yjb23tech@gmail.com>
2023-07-08 23:50:33 +0100

    Did some great work today

--- rest_api_lab.py
+++ rest_api_lab.py
@@ -1,1 +1,18 @@
-print("Learning with Caleb Curry")
+#print("Learning with Caleb Curry")
+import requests 
+import json 
+
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+#peel off the topmost layer 
+#each go round of the 'for' loop sees the key be mapped into the data variable
+for data in response.json()['items']:
+    #If the key mapped into the 'data' variable matches the index 'title' the associated value with the key is printed 
+    print(data['title'])
+
+print()
+
+for data in response.json()['items']:
+    print(data['content_license'])
+
+
